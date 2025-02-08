cd9c1803bc7ed9c53e618f67cf9dbc561b6a8d12
mausamtechno <mausam@technoheaven.net>
2025-02-08 12:07:41 +0530

    remove unused file

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { fetchData } from "./app/utils";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-//   console.log(request, "Print request inside middleware");
-  const res = await fetchData({ id: 1 });
-  console.log(res, "API response inside middleware");
   return NextResponse.next();
 }
 
